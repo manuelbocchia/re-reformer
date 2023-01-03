3db82516590bf434d3ab713f13b24c38f259cb9f
Manuel Bocchia <zerounodos@gmail.com>
2023-01-03 14:29:32 -0300

    added user creation, have to fix editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,29 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def show
+    end
+
     def create
        #@user = User.new( username: params[:username], email: params[:email], password: params[:password])
 
        @user = User.new(user_params)
 
         if @user.save
-            redirect_to users_path
+            redirect_to users_path(@user.id)
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        @user = User.find(user_params[:id])
+
+        if @user.update(user_params)
+            redirect_to users_path(@user.id)
         else
             render :new, status: :unprocessable_entity
         end
